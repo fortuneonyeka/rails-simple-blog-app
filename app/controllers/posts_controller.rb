16232b2a5e14f043784bdd46ca87d5fee4474b34
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-26 13:54:10 +0100

    added edit,update and destroy controllers and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  
 
   def create
     # render plain: params[:post].inspect
@@ -21,6 +22,27 @@ class PostsController < ApplicationController
       render "new"
     end
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+     @post.destroy
+      redirect_to posts_path
+  end
+
   private def post_params
     params.require(:post).permit(:title, :body)
   end
